60d99e203bba3435e12ac24319aec36899513511
yuanliming <549750956@qq.com>
2021-04-28 14:35:45 +0800

    feat search item

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,29 +4,36 @@ import axios from "axios";
 import "./index.css";
 export default class Header extends Component {
   search = () => {
-    const { value } = this.input;
-    this.props.updateState({
-      isFirst: false,
-      isLoading: true,
-    });
-    axios({
-      method: "GET",
-      url: `https://api.github.com/search/users?q=${value}`,
-    }).then(
-      (response) => {
-        const { items } = response.data;
-        this.props.updateState({
-          users: items,
-          isLoading: false,
-        });
-      },
-      (err) => {
-        this.props.updateState({
-          isLoading: false,
-          err: err.message,
-        });
-      }
-    );
+    const { updateState } = this.props;
+    const value = this.input.value.trim();
+    if (!value) {
+      alert("请勿输入空信息");
+      this.input.value = "";
+      return;
+    } else {
+      updateState({
+        isFirst: false,
+        isLoading: true,
+      });
+      axios({
+        method: "GET",
+        url: `https://api.github.com/search/users?q=${value}`,
+      }).then(
+        (response) => {
+          const { items } = response.data;
+          updateState({
+            users: items,
+            isLoading: false,
+          });
+        },
+        (err) => {
+          updateState({
+            isLoading: false,
+            err: err.message,
+          });
+        }
+      );
+    }
   };
   render() {
     return (
@@ -34,6 +41,7 @@ export default class Header extends Component {
         <h1>Search Github Users</h1>
         <div className="inputWrap">
           <input
+            onKeyUp={(e) => e.keyCode === 13 && this.search()}
             ref={(c) => (this.input = c)}
             type="text"
             placeholder="please input a username"
